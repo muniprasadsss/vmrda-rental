1835400411d876010b3cb97f0cf06f9e68ebf2c9
Srivatsa Kidami Santhanam <srivatsaks@sssbitech.com>
2024-09-04 14:59:40 +0530

    'apiurl'

--- src/app/services/userService/user-service.service.ts
+++ src/app/services/userService/user-service.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../configuration';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
+  private apiUrl = environment.apiUrl; // Backend API URL
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUserDetails():Observable<any>{
+    return this.http.get(`${this.apiUrl}/users`)
+  }
+
 }
--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserServiceService } from '../services/userService/user-service.service';
 
 @Component({
   selector: 'app-user-details',
@@ -7,8 +9,29 @@ import { Component } from '@angular/core';
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.scss'
 })
-export class UserDetailsComponent {
-
-
+export class UserDetailsComponent implements OnInit {
+  dataSource:any=''
+  responseMsg: any;
+  constructor(private router:Router,private userdetailsservice:UserServiceService){}
+ngOnInit(): void {
+  this.getuserdetails()
+}
+  getuserdetails() {
+    console.log("api entered..."); 
+    this.userdetailsservice.getUserDetails().subscribe({
+      next: (res: any) => {
+        this.dataSource = Object.keys(res).map(key => ({ ...res[key] }));
+        this.responseMsg = res.message;
+        console.log(this.dataSource, "userservice data...");
+      },
+      error: (err: any) => {
+        if (err.error?.message) {
+          this.responseMsg = err.error?.message;
+        } else {
+          this.responseMsg = "error";
+        }
+      }
+    });
+  }
   
 }
