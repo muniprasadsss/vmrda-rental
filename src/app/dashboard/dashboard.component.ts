20910db631eb8d1a2e4a07ee239219f83dd0aad6
muni prasad <muniprasadpulicherla@sssbitech.com>
2024-09-05 10:01:30 +0530

    'complexuialignment'

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,12 +3,11 @@ import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { RouterOutlet } from '@angular/router';
 import { ComplexDetailsComponent } from '../complex-details/complex-details.component';
 import { UserDetailsComponent } from '../user-details/user-details.component';
-import { HttpClientModule } from '@angular/common/http';
-HttpClientModule
+
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [PrimeNgModule,RouterOutlet,ComplexDetailsComponent,UserDetailsComponent,HttpClientModule],
+  imports: [PrimeNgModule,RouterOutlet,ComplexDetailsComponent,UserDetailsComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
