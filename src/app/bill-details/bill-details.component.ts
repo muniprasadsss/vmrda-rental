1b212bdbed76eb5f9fe8701c87d497e5407d3aa3
Srivatsa Kidami Santhanam <srivatsaks@sssbitech.com>
2024-09-06 10:20:30 +0530

    'billdetailsui'

--- src/app/bill-details/bill-details.component.ts
+++ src/app/bill-details/bill-details.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
+import { HeaderComponent } from '../header/header.component';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-bill-details',
   standalone: true,
-  imports: [PrimeNgModule],
+  imports: [PrimeNgModule,HeaderComponent,DashboardComponent,FooterComponent],
   templateUrl: './bill-details.component.html',
   styleUrl: './bill-details.component.scss'
 })
