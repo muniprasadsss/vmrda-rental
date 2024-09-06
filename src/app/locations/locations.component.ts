5d5807b4de42363686197af48dfc844967d5d93f
muni prasad <muniprasadpulicherla@sssbitech.com>
2024-09-06 10:07:28 +0530

    'routing'

--- src/app/locations/locations.component.ts
+++ src/app/locations/locations.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
+import { FooterComponent } from "../footer/footer.component";
+import { DashboardComponent } from "../dashboard/dashboard.component";
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-locations',
   standalone: true,
-  imports: [PrimeNgModule],
+  imports: [PrimeNgModule, FooterComponent, DashboardComponent, HeaderComponent],
   templateUrl: './locations.component.html',
   styleUrl: './locations.component.scss'
 })
