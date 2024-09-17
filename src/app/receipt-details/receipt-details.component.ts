b33cb8c8e28921107431980f35aad73fd474d792
muni prasad <muniprasadpulicherla@sssbitech.com>
2024-09-17 11:57:22 +0530

    receipt api data

--- src/app/receipt-details/receipt-details.component.ts
+++ src/app/receipt-details/receipt-details.component.ts
@@ -3,6 +3,7 @@ import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { HeaderComponent } from '../header/header.component';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { FooterComponent } from '../footer/footer.component';
+import { ReceptDetailsService } from '../services/receptDetails/recept-details.service';
 
 @Component({
   selector: 'app-receipt-details',
@@ -13,9 +14,29 @@ import { FooterComponent } from '../footer/footer.component';
 })
 export class ReceiptDetailsComponent {
   visible: boolean = false;
+  userRole:any
+  userID:any
+  receiptData:any
+  constructor(private http:ReceptDetailsService){}
 
+  ngOnInit(){
+    this.userRole = localStorage.getItem('role')
+    this.userID = localStorage.getItem('userId')
+    this.getReceptData();
+  }
   showDialog() {
     this.visible = true;
 }
+
+  getReceptData(){
+    this.http.getReciptDetails(this.userID,this.userRole).subscribe({
+      next:(res:any)=>{
+        this.receiptData = res.receiptData
+      },
+      error:(err:any)=>{
+        
+      }
+    })
+  }
 
 }
--- src/app/services/receptDetails/recept-details.service.ts
+++ src/app/services/receptDetails/recept-details.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../configuration';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReceptDetailsService {
+    apiUrl = environment.apiUrl
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getReciptDetails(USER_ID: string,USER_TYPE:string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/getReceiptInfo`,{USER_ID,USER_TYPE});
+  }
 }
