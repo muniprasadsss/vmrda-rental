6e53f22cd82470f1e3268ae9f264f4cb47572d89
Srivatsa Kidami Santhanam <srivatsaks@sssbitech.com>
2024-09-20 18:28:32 +0530

    profil chnges

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,53 +1,129 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthGuardsService } from '../services/authGuards/auth-guards.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [PrimeNgModule,FormsModule],
+  imports: [PrimeNgModule, FormsModule,ReactiveFormsModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  selectedCity:any
-  userType:any
-  dropdownDiv:boolean=false
+  userType: any;
+  dropdownDiv: boolean = false;
   items: any | undefined;
+  userdetails: any;
+  viewProfileDialog: boolean = false; // For viewing profile dialog
+  changePasswordDialog: boolean = false; // For changing password dialog
+  profileForm: FormGroup;
+  passwordform: FormGroup;
 
-  constructor(private router:Router, private authService:AuthGuardsService){}
+  // formData: any = {
+  //   USER_ID: "",
+  //   USER_NAME: "",
+  //   MOBILE_NUM: "",
+  //   EMAIL_ID: "",
+  //   user_type: "",
+  //   Revenue_Division:"",
+  //   gst_in:"",
+  //   aadhar_no:"",
+  //   pan_no:"",
+  //   nature_of_bussiness:"",
+  // };
+
+
+  constructor(private router: Router, private authService: AuthGuardsService,private fb: FormBuilder) {
+    this.profileForm = this.fb.group({
+      USER_ID: [{ value: '', disabled: true }, Validators.required],
+      USER_NAME: [{ value: '', disabled: true }, Validators.required],
+      USER_TYPE: [{ value: '', disabled: true }, Validators.required],
+      REVENUE_DIVISION: [{ value: '', disabled: true }, Validators.required],
+      EMAIL_ID: [{ value: '', disabled: true }, [Validators.required, Validators.email]],
+      MOBILE_NUM: [{ value: '', disabled: true }, Validators.required],
+      AADHARNO: [{ value: '', disabled: true }, Validators.required],
+      GST_IN: [{ value: '', disabled: true }, Validators.required],
+      PAN: [{ value: '', disabled: true }, Validators.required],
+      NATURE_OF_BUSINESS: [{ value: '', disabled: true }, Validators.required],
+      // password: [{ value: '' }, Validators.required],
+      // confirmPassword: [{ value: '' }, Validators.required],
+      // Add more fields as necessary
+    });
+    this.passwordform = this.fb.group({
+      Password: [{ value: '' }, Validators.required],
+      ChangePassword: [{ value: '' }, Validators.required],
+      // password: [{ value: '' }, Validators.required],
+      // confirmPassword: [{ value: '' }, Validators.required],
+      // Add more fields as necessary
+    });
+  }
+
   ngOnInit(): void {
-
     this.items = [
       {
-          // label: 'Options',
-          items: [
-              {
-                  label: 'Edit Profile',
-                  icon: 'pi pi-pencil',
-                  // routerLink: ['/edit'] 
-              },
-              {
-                  label: 'Log Out',
-                  icon: 'pi pi-sign-out',
-                  // routerLink: ['/'] ,
-                  
-                  command: () => this.logOut(),
-              }
-          ]
+        items: [
+          {
+            label: 'View Profile',
+            icon: 'pi pi-eye',
+            command: () => this.showViewProfileDialog(),
+          },
+          {
+            label: 'Change Password',
+            icon: 'pi pi-key',
+            command: () => this.showChangePasswordDialog(),
+          },
+          {
+            label: 'Log Out',
+            icon: 'pi pi-sign-out',
+            command: () => this.logOut(),
+          }
+        ]
       }
-  ];
-   this.userType=localStorage.getItem("userId")
+    ];
+    this.passwordform = this.fb.group({
+      Password: [{ value: '' }, Validators.required],
+      ChangePassword: [{ value: '' }, Validators.required],
+      // password: [{ value: '' }, Validators.required],
+      // confirmPassword: [{ value: '' }, Validators.required],
+      // Add more fields as necessary
+    });
+    
+    this.userType = localStorage.getItem("userId");
+    this.userdetails=localStorage.getItem("userInfo")
+
+    // Load user profile data from localStorage
+    const userInfo = localStorage.getItem("userInfo");
+    console.log(userInfo,"userinfo...");
+    if (userInfo) {
+      const userDetails = JSON.parse(userInfo);
+      this.profileForm.patchValue({
+        USER_NAME: userDetails.USER_NAME ,
+        USER_ID: userDetails.USER_ID ,
+        USER_TYPE: userDetails.user_type ,
+        REVENUE_DIVISION: userDetails.REVENUE_DIVISION ,
+        EMAIL_ID: userDetails.EMAIL_ID ,
+        MOBILE_NUM: userDetails.MOBILE_NUM ,
+        AADHARNO: userDetails.AADHARNO ,
+        GST_IN: userDetails.GST_IN ,
+        PAN: userDetails.PAN ,
+        NATURE_OF_BUSINESS: userDetails.NATURE_OF_BUSINESS ,
+        // password: userDetails.Password ,
+      });
+    }
   }
-  showDropDown(){
-    this.dropdownDiv=true
+
+  showViewProfileDialog() {
+    this.viewProfileDialog = true; // Show profile dialog
   }
-  logOut(){
+
+  showChangePasswordDialog() {
+    this.changePasswordDialog = true; // Show change password dialog
+  }
+
+  logOut() {
     this.authService.logout();
-    // localStorage.setItem("userType",'')
-   this.router.navigateByUrl('/')
+    this.router.navigateByUrl('/');
   }
-  
 }
