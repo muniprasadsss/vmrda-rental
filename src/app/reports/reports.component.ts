e42cf2f025efd0583b163ea2753060f1ea1b981e
muni prasad <muniprasadpulicherla@sssbitech.com>
2025-01-20 15:11:26 +0530

    SECRETARY reports issue fixed

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -20,7 +20,7 @@ ngOnInit(): void {
 
   this.userType=localStorage.getItem('userId')
 
-  if(this.userType === 'AO' || this.userType === 'SECARATORY' || this.userType === 'COMMISSIONER' || this.userType === 'ADMIN' || this.userType === 'CO_ADMIN'){
+  if(this.userType === 'AO' || this.userType === 'SECRETARY' || this.userType === 'COMMISSIONER' || this.userType === 'ADMIN' || this.userType === 'CO_ADMIN'){
     this.reportUrl = 'https://app.powerbi.com/view?r=eyJrIjoiYzBlYzBmOTUtMmFiZi00NWY0LTk4MWItYTBjZDYxODE2ZDE2IiwidCI6IjM5MmFlNDIzLWFiZTktNDIxOS1iMTU5LTBhZTc3YWMxOWZjYSJ9'
   }
   else if(this.userType === 'RI-1'){
