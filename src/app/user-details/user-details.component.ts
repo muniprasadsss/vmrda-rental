f3cad01fefc3e64a6e8fc3ded6eb1fae9cfea08d
Srivatsa Kidami Santhanam <srivatsaks@sssbitech.com>
2025-01-29 17:25:48 +0530

    userdetails user image security attachment added

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -9,6 +9,7 @@ import { FooterComponent } from "../footer/footer.component";
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import * as XLSX from 'xlsx';
+import { ChangeRequestService } from '../services/changeRequest/change-request.service';
 
 
 @Component({
@@ -35,9 +36,15 @@ export class UserDetailsComponent implements OnInit {
   editVisible: boolean = false; // For Edit dialog visibility
   editForm!: FormGroup; // Form group for Edit dialog
   selectedUser: any; // Store selected user data
+  securityAttachment: any;
+  securityattachmentUrl:any = null;
+  userPhoto:any;
+  userphotoUrl:any=null;
+
+
 
   constructor(private router: Router, private userdetailsservice: UserServiceService,
-              private fb: FormBuilder, private toasterservice: ToastrService)
+              private fb: FormBuilder, private toasterservice: ToastrService,private Http: ChangeRequestService,)
   {
     this.addNewForm = this.fb.group({
       username: [null, Validators.required],
@@ -47,6 +54,7 @@ export class UserDetailsComponent implements OnInit {
       pan: [null],
       email_id: [null],
       gstIn: [null],
+      remarks: [null],
     });
     this.editForm = this.fb.group({
       editUsername: [null, Validators.required],
@@ -56,7 +64,8 @@ export class UserDetailsComponent implements OnInit {
       editPan: [null],
       editEmail: [null],
       editGstin: [null],
-      editRevenue: [null, Validators.required]
+      editRevenue: [null, Validators.required],
+      editRemarks: [null],
     });
   }
 
@@ -70,6 +79,8 @@ export class UserDetailsComponent implements OnInit {
     this.userdetailsservice.getUserDetailsByRole(this.userID, this.userRole).subscribe({
       next: (res: any) => {
         this.dataSource = res.userinfo;
+        console.log(this.dataSource.length,'0909');
+        
         this.responseMsg = res.message;
       },
       error: (err: any) => {
@@ -103,7 +114,10 @@ export class UserDetailsComponent implements OnInit {
         revenueDivision: null,
         natureOfBusiness: this.addNewForm.value.natureOfBusiness,
         pan: this.addNewForm.value.pan,
-        gstIn: this.addNewForm.value.gstIn
+        gstIn: this.addNewForm.value.gstIn,
+        user_image:this.userphotoUrl ,
+        security_url:this.securityattachmentUrl,
+        remarks: this.addNewForm.value.remarks
       }
       this.userdetailsservice.createUser(payload).subscribe({
         next: (response: any) => {
@@ -129,14 +143,15 @@ export class UserDetailsComponent implements OnInit {
   openEditDialog(user: userdetails) {
     this.selectedUser = user;
     this.editForm.patchValue({
-      editUsername: user.USER_NAME,
-      editMobile: user.MOBILE_NUM,
-      editBusiness: user.NATURE_OF_BUSINESS,
-      editAadhar: user.Aadhaar_No,
-      editPan: user.PAN,
-      editEmail: user.EMAIL_ID,
-      editGstin: user.GST_IN,
-      editRevenue: user.REVENUE_DIVISION
+      editUsername: this.selectedUser.USER_NAME,
+      editMobile: this.selectedUser.MOBILE_NUM,
+      editBusiness: this.selectedUser.NATURE_OF_BUSINESS,
+      editAadhar: this.selectedUser.Aadhaar_No,
+      editPan: this.selectedUser.PAN,
+      editEmail: this.selectedUser.EMAIL_ID,
+      editGstin: this.selectedUser.GST_IN,
+      editRevenue: this.selectedUser.REVENUE_DIVISION,
+      editRemarks:this.selectedUser.remarks
       // Update this field name as per your data
     });
     this.editVisible = true;
@@ -161,6 +176,9 @@ export class UserDetailsComponent implements OnInit {
       gstIn: editFormData.editGstin,
       userType: editFormData.user_type,
       revenueDivision: editFormData.editRevenue,
+      user_image:this.userphotoUrl,
+      security_url:this.securityattachmentUrl,
+      remarks: editFormData.editRemarks
     };
   
     this.userdetailsservice.editUserDetails(payload).subscribe({
@@ -193,10 +211,10 @@ export class UserDetailsComponent implements OnInit {
   }
 
   // This method ensures the input value is always uppercase
-convertToUpperCase(event: Event): void {
-  const inputElement = event.target as HTMLInputElement;
-  inputElement.value = inputElement.value.toUpperCase();
-}
+  convertToUpperCase(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    inputElement.value = inputElement.value.toUpperCase();
+  }
 
   validatePanInput(event: KeyboardEvent) {
     const key = event.key;
@@ -232,6 +250,74 @@ convertToUpperCase(event: Event): void {
     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1'); // Append the sheet to the workbook
     XLSX.writeFile(wb, 'tenants-data.xlsx'); // Write the file
 
+  }
+
+  onsecurityattachmentChange(event: any) {
+    const files = event.target.files; // Get the list of selected files
+    if (files && files.length > 0) { // Check if any file is selected
+        const file = files[0]; // Get the first file
+        this.securityAttachment = file;
+        this.uploadsecurityAttachment(); // Trigger upload function
+    } else {
+        console.warn('No file selected');
+    }
+  }
+
+  uploadsecurityAttachment(){
+    let fd = new FormData();
+    fd.append('image',  this.securityAttachment);
+    this.Http.uploadAttachment(fd).subscribe({
+      next:(res:any)=>{
+        this.securityattachmentUrl = res.location;
+        console.log(this.securityattachmentUrl,'securityattachmentUrl');
+        this.securityAttachment = null;
+      },
+      error:(err:any)=>{
+      }
+    })
+  }
+
+  onuserphotoChange(event: any) {
+    const files = event.target.files; // Get the list of selected files
+    if (files && files.length > 0) { // Check if any file is selected
+        const file = files[0]; // Get the first file
+        this.userPhoto = file;
+        this.uploaduserPhoto(); // Trigger upload function
+    } else {
+        console.warn('No file selected');
+    }
+  }
+
+  uploaduserPhoto(){
+    let fd = new FormData();
+    fd.append('image',  this.userPhoto);
+    this.Http.uploadAttachment(fd).subscribe({
+      next:(res:any)=>{
+        this.userphotoUrl = res.location;
+        console.log(this.userphotoUrl,'userphotoUrl');
+        this.userPhoto = null;
+      },
+      error:(err:any)=>{
+      }
+    })
+  }
+
+  downloaduserImage(url: string) {
+    if (url) {
+      // Open the S3 URL in a new tab
+      window.open(url, '_blank');
+    } else {
+      console.error('No attachment URL provided');
+    }
+  }
+
+  downloadsecurityAttachment(url: string) {
+    if (url) {
+      // Open the S3 URL in a new tab
+      window.open(url, '_blank');
+    } else {
+      console.error('No attachment URL provided');
+    }
   }
 
 }
