b174331294d7fd93be2256d451c2b9e4c7587264
Muni Prasad Pulicherla <muniprasadpulicherla@sssbitech.com>
2025-01-30 16:21:31 +0530

    changed deploy file for pipeline

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,12 +8,6 @@ cd /var/www/html
 # Remove old files
 sudo rm -rf *
 
-# Unzip the new frontend build
-sudo unzip /var/www/html/frontend.zip -d /var/www/html
-
-# Remove the ZIP file after extraction
-sudo rm -f /var/www/html/frontend.zip
-
 # Set correct permissions
 sudo chown -R www-data:www-data /var/www/html
 sudo chmod -R 755 /var/www/html
