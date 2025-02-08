f60120ab3c1116ee774c1b1ddc60d64c25e458e0
Muni Prasad Pulicherla <muniprasadpulicherla@sssbitech.com>
2025-02-08 13:13:34 +0530

    fixes

--- frontend/src/app/services/configuration.ts
+++ frontend/src/app/services/configuration.ts
@@ -4,7 +4,7 @@ export const environment = {
     // test-envi
     // apiUrl: 'http://vmrdatest.ap-south-1.elasticbeanstalk.com/'
 
-    // apiUrl: 'http://13.126.220.32:3000'
+    // apiUrl: 'http://13.203.181.236:3000'
     // apiUrl: 'http://localhost:3000'
 
     // vmrdarental.com domain 
