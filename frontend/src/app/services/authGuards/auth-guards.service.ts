c524e33d49f98e40c6bb72ec7f06dbcf28debcc9
Muni Prasad Pulicherla <muniprasadpulicherla@sssbitech.com>
2025-02-14 18:18:13 +0530

    enabled add receipt

--- frontend/src/app/services/authGuards/auth-guards.service.ts
+++ frontend/src/app/services/authGuards/auth-guards.service.ts
@@ -78,6 +78,7 @@ export class AuthGuardsService implements CanActivate {
         this.sessionMessageSource.next(message); // Set the message
         this.router.navigate(['session-expired']);
         this.stopTokenValidationCheck();
+        this.logout();
       }
     }
     )}
